1f1acf4286dd5b7a8cc50083c8a4c74d6c7439c4
Moody Romolino <moromolino@gmail.com>
2014-10-20 16:40:39 -0400

    Delete globals.h

--- Source/globals.h
+++
@@ -1,4 +0,0 @@
-const bool SHOW_DEBUG_CONSOLE = false;	//set this to true to display a console with your program that you can use standard c IO calls to
-
-const int MAX_SONGS = 100;		//The maximum number of songs on disk that we can manage
-const int MAX_PLAYLIST = 200;	//The maximum number of songs that can be in a playlist
