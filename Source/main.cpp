776a8406da82989183f9511b62fe7477d68d1233
Moody Romolino <moromolino@gmail.com>
2014-10-20 16:53:14 -0400

    Update main.cpp
    fixed some comment errors

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,16 @@
 #include "stdafx.h"  //needed for the Windows display
 #include "globals.h"  //some global variables are included here
 #include <cstdlib>	//standard c library
-#include "mediaItem.h"  //the mediaItem class  (you need to write a class that extends this class)
+#include "mediaItem.h"  //the mediaItem class
 #include "song.h"
 #include <fstream>
 #include <string>
 using namespace std;
 
-//declare global variables here (your song and playlist arrays)
+//Global declarations
 ifstream file;
 song songname[MAX_SONGS];
 int songplaylist[MAX_PLAYLIST];
-
-//as well as ints describing the current number of entries in each of these)
 int number_of_songs = 0;
 int counter = 0;
 
@@ -27,7 +25,7 @@ bool loadSongList(string filename)
 	if (file.is_open())
 	{
 		file >> number_of_songs; // number of songs.
-		file.ignore(); // ignore the first line (number of songs = 33) from text file.
+		file.ignore(); // ignore the first line (number of songs) from text file.
 		while (file.good()) // gets next line
 		{
 			getline(file, artist[a]); //reads line by line.
@@ -91,7 +89,7 @@ void addSongToPlaylist(int songNum)
 
 int getNumberOfSongsInPlaylist()
 {
-	return counter; // amount of sonds in playlist.
+	return counter; // amount of songs in playlist.
 }
 
 void removeSongFromPlaylist(int playlistSpot)
